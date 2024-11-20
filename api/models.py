cba3682357b8c996378b29568f39abfd20ed3096
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-11-21 00:25:51 +0100

    viewsets definidos

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class User(models.Model):
+    idUser = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(null=False)
+
+
+    def str(self):
+        return self.username
+
