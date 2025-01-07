e7addb8ad220d1e5c9abe776585106655be2ee87
juan <jdeomoya@gmail.com>
2025-01-07 16:45:22 +0100

    Creado el modelo de usuario y he creado un requirements.txt para solucionar el problema de las dependencias

---
+++ api/Models/UserModel.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models import Model
+class User(models.Model):
+    idUser = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, null=False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.CharField(null=False)
+
+
+    def str(self):
+        return self.username
