312025701119a22f0205e1ba1befd64198c2c3a2
juan <jdeomoya@gmail.com>
2025-01-08 11:50:47 +0100

    Añadidos cambios en el control de errores en el modelo de usuario

--- api/Models/UserModel.py
+++ api/Models/UserModel.py
@@ -1,11 +1,16 @@
 from django.db import models
-from django.db.models import Model
+from rest_framework.exceptions import ValidationError
+
+
 class User(models.Model):
-    idUser = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=100, null=False)
-    email = models.EmailField(unique=True, null=False)
-    password = models.CharField(null=False)
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, null=False, verbose_name="Nombre")
+    email = models.EmailField(unique=True, null=False, verbose_name="Correo Electrónico")
+    password = models.CharField(max_length=128, null=False, verbose_name="Contraseña")
 
+    def clean(self):
+        if not all([self.nombre, self.email, self.password]):
+            raise ValidationError('Todos los campos deben estar rellenados correctamente.')
 
-    def str(self):
-        return self.username
+    def __str__(self):
+        return self.nombre
